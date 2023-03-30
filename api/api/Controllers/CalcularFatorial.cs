919104bc5433431e05f74e52d512125fa2c63213
Natan Oliveira Martins <85328929+CariocaMartins@users.noreply.github.com>
2023-03-30 17:49:32 -0300

    Add files via upload

---
+++ api/api/Controllers/CalcularFatorial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CalcularFatorial : ControllerBase
+    {
+
+        [HttpGet]
+        public string Calcular(int number)
+        {
+            int fatorial = 1;
+            string conta = "1";
+
+            for (int i = 2; i <= number; i++)
+            {
+                fatorial *= i;
+                conta += " * " + i;
+            }
+
+            return $"{conta} = {fatorial}";
+        }
+
+    }
+
+}
